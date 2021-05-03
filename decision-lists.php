4ad4212429aabc2a90c604c6b539884965d63b7b
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:23:51 -0700

    Fixes issues where extended plugins did not get registered items.

--- decision-lists.php
+++ decision-lists.php
@@ -4,15 +4,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
+add_action( 'underpin/before_setup', function ( $instance ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/abstracts/Decision.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/abstracts/registries/Decision_List.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/loaders/Decision_Lists.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/factories/Decision_Instance.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/factories/Decision_List_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'decision_lists', [
+		$instance->loaders()->add( 'decision_lists', [
 			'registry' => 'Underpin_Decision_Lists\Loaders\Decision_Lists',
 		] );
-	}
-}, 4 );
+}, 3 );
