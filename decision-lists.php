0a1bfa59d8f1f09583da4843d39a7d667deea2a0
Alex Standiford <a@alexstandiford.com>
2021-05-05 17:15:32 -0700

    Corrects underpin autoloader

--- decision-lists.php
+++ decision-lists.php
@@ -4,13 +4,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file ) {
+add_action( 'underpin/before_setup', function ( $file, $class ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/abstracts/Decision.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/abstracts/registries/Decision_List.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/loaders/Decision_Lists.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/factories/Decision_Instance.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/factories/Decision_List_Instance.php' );
-		Underpin\underpin()->get( $file )->loaders()->add( 'decision_lists', [
+		Underpin\underpin()->get( $file, $class )->loaders()->add( 'decision_lists', [
 			'registry' => 'Underpin_Decision_Lists\Loaders\Decision_Lists',
 		] );
-}, 3 );
+}, 3, 2 );
